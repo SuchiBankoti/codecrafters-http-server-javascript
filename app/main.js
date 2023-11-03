e8b7453e0bf18a6079841dd1109d0a90e0bb41ae
suchi bankoti <suchibankoti95@gmail.com>
2023-11-03 17:18:27 +0530

    respond

--- app/main.js
+++ app/main.js
@@ -1,10 +1,24 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
+const get_method_path_protocol = (str) => {
+    const arr = str.split(' ')
+    const method = arr[0]
+    const path = arr[1]
+    const protocol = arr[2]
+    return [method,path,protocol]
+}
 const server = net.createServer((socket) => {
+
+    socket.on('data', (data) => {
+        const lines = data.toString().split('\n')
+        const [method,path,protocol]=get_method_path_protocol(lines[0])
+        console.log('method:', method, "path:", path, "protocol:", protocol)
+        socket.write(`${protocol} 200 OK\r\n\r\n`)
+        socket.end()
+   })
+
+
   socket.on("close", () => {
     socket.end();
     server.close();
