d0ccc0fc33387df0a75aa82a71364dd1b98c4ea1
suchi bankoti <suchibankoti95@gmail.com>
2023-11-03 18:02:40 +0530

    check path return string body

--- app/main.js
+++ app/main.js
@@ -8,21 +8,31 @@ const get_method_path_protocol = (str) => {
     const protocol = arr[2]
     return [method,path,protocol]
 }
+const get_body = (path) => {
+    const body = path.split("/")
+    return body[2]
+}
+
 const server = net.createServer((socket) => {
 
     socket.on('data', (data) => {
         const lines = data.toString().split('\n')
         const [method,path,protocol]=get_method_path_protocol(lines[0])
         console.log('method:', method, "path:", path, "protocol:", protocol)
+        const body=get_body(path)
+        console.log('path',path)
         let response=''
         if (path === "/") {
             response="HTTP/1.1 200 OK\r\n\r\n"
-        } else {
+        } else if (path.startsWith("/echo")) {
+            response=`HTTP/1.1 200 OK\r\n\r\nContent-Type:text/plain\r\n\r\nContent-Length:${body.length}\r\n\r\n${body}`
+        }
+        else {
             response="HTTP/1.1 404 Not Found\r\n\r\n"
         }
-        
+        console.log(response)
         socket.write(response)
-        socket.end()
+        // socket.end()
    })
 
 
